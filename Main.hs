25794bb9b6eb883a915b113cbf838d09a3cc7fd8
Evžen Wybitul <wybitul.evzen@gmail.com>
2019-11-06 23:59:06 +0100

    Remove unwanted code

--- Main.hs
+++ Main.hs
@@ -28,22 +28,6 @@ while i<j:
     i=i+1
 |]
 
-def displayMsg(msg, x): {
-    print(msg + string(x))
-}
-
-i = 0
-j = readLine()
-while i < j: {
-    if even(i): {
-        displayMsg("Even: ", i)
-    }
-    else: {
-        displayMsg("Not even: ", i)
-    }
-    i = i + 1
-}
-
 parseShow :: PDoc a => Parser a -> String -> IO ()
 parseShow prs str = do
   let (Right ast) = parse prs "" str
